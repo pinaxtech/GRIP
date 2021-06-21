06af73a518ed67734822eb6d5c7895e2d95c995d
Pinakesh roy <adityaroy66666@gmail.com>
2021-06-22 01:38:34 +0530

    Add files via upload

---
+++ spec.py
@@ -0,0 +1,31 @@
+block_cipher = None
+
+
+a = Analysis(['Task_2_Color_Detection.py'],
+             pathex=['C:\Users\Ronobrata Paul\Downloads\Color_Detection'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='Color_Detection',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=False )
